208ce1162c8b6acad9033a7f316538ea1be280a7
Michael Ashe <mickjohnashe@hotmail.com>
2017-10-20 22:04:39 +0100

    Updating versions

--- src/event.rs
+++ src/event.rs
@@ -1,4 +1,5 @@
-use chrono::{DateTime, UTC, Local};
+use chrono::{DateTime, Local};
+use chrono::prelude::*;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub struct Event {
@@ -6,14 +7,14 @@ pub struct Event {
     pub round: u64,
     pub country: String,
     pub location: String,
-    pub start_date: DateTime<UTC>,
-    pub end_date: DateTime<UTC>,
+    pub start_date: DateTime<Utc>,
+    pub end_date: DateTime<Utc>,
     pub sessions: Vec<Session>,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub struct Session {
    pub name: String,
-   pub date: DateTime<UTC>,
+   pub date: DateTime<Utc>,
    pub time: Option<DateTime<Local>>,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![feature(plugin)]
 #[macro_use] extern crate serde_derive;
+extern crate serde_json;
+extern crate serde;
 extern crate chrono;
 
 pub mod event;
