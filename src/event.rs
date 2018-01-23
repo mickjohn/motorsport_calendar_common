a3404b07199aa4d026c71f3f2ae914681a156254
Michael Ashe <mickjohnashe@hotmail.com>
2018-01-23 21:34:41 +0000

    Removing start and end date from Event

--- src/event.rs
+++ src/event.rs
@@ -3,18 +3,21 @@ use chrono::prelude::*;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub struct Event {
+    pub id: i32,
     pub sport: String,
-    pub round: u64,
+    pub round: i32,
     pub country: String,
     pub location: String,
-    pub start_date: DateTime<Utc>,
-    pub end_date: DateTime<Utc>,
+    // pub start_date: DateTime<Utc>,
+    // pub end_date: DateTime<Utc>,
     pub sessions: Vec<Session>,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub struct Session {
+   pub id: i32,
+   pub event_id: i32,
    pub name: String,
    pub date: DateTime<Utc>,
-   pub time: Option<DateTime<Local>>,
+   pub time: Option<DateTime<Utc>>,
 }
